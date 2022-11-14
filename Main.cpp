c0030bc781af6268ee38ff855e48c0bae6332824
utrachet <utrachet@gaming.tech>
2022-11-14 14:22:03 +0100

    Test

--- Main.cpp
+++ Main.cpp
@@ -40,7 +40,7 @@ int main(int argc, char* args[])
             screenSurface = SDL_GetWindowSurface(window);
 
             //Fill the surface white
-            SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+            SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 250, 120, 200));
 
             //Update the surface
             SDL_UpdateWindowSurface(window);
