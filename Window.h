dd8c7cab656562b9c90e3ee8cd8411057551eae4
utrachet <utrachet@gaming.tech>
2022-11-15 13:32:18 +0100

    Stable version

---
+++ Window.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <SDL.h>
+#include <stdio.h>
+
+class MainSDLWindow
+{
+public:
+	MainSDLWindow(void);
+	~MainSDLWindow(void);
+
+	int Init(const char* Jesus, int width, int height);
+	//SDL_Renderer* GetRenderer(void);
+
+private:
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+};
